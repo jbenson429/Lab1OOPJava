88fce6fc00035c44308bf190b7290c275bb748ed
Josephine Benson <JBENSON6@cub.uca.edu>
2025-01-24 00:17:51 -0800

    initial commit

--- src/Register.java
+++ src/Register.java
@@ -2,11 +2,13 @@ public class Register {
     private static final Denomination[] DENOMINATIONS = {
             new Denomination("Hundred-Dollar Note", 100.0, "bill", "hundred_dollar.png"),
             new Denomination("Fifty-Dollar Note", 50.0, "bill", "fifty_dollar.png"),
+            new Denomination("Twenty-Dollar Note", 20.0, "bill", "twenty_dollar.png"),
             new Denomination("Ten-Dollar Note", 10.0, "bill", "ten_dollar.png"),
             new Denomination("Five-Dollar Note", 5.0, "bill", "five_dollar.png"),
             new Denomination("One-Dollar Note", 1.0, "bill", "one_dollar.png"),
             new Denomination("Quarter", 0.25, "coin", "quarter.png"),
             new Denomination("Dime", 0.10, "coin", "dime.png"),
+            new Denomination("Nickel", 0.05, "coin", "nickel.png"),
             new Denomination("Penny", 0.01, "coin", "penny.png")
     };
 
